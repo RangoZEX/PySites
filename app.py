4d95dd2161e69f2963e02767d32ee496c13265b6
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-10-17 00:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from app import create_app
+from flask import Flask
+from dotenv import load_dotenv
+import os
 
-app = create_app()
+# Load environment variables from config.env
+load_dotenv('config.env')
+
+app = Flask(__name__)
+
+# Access the BASE_URL from environment variables
+BASE_URL = os.getenv('BASE_URL')
+
+@app.route('/')
+def home():
+    return f"Base URL is: {BASE_URL}"
 
 if __name__ == '__main__':
     app.run(debug=True)
