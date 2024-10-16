e37b8aba0397e44771d82cece8bffe9bd3b6f47e
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-10-17 00:45:01 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
-####
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    # Import routes from the routes module
+    from .routes import home_route
+    app.register_blueprint(home_route)
+
+    return app
