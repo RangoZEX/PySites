27451564f39d8b627432638398ca5ff88f55f96c
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-10-17 11:18:41 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ HTML_TEMPLATE = '''
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Welcome to AMC Dev</title>
+    <title>AMC DEVELOPERS</title>
     <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet"> <!-- Nova Square font -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet"> <!-- Digital font -->
     <style>
