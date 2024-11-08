15096dcd35f5b1fc2e57433a672e40626b0be20a
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-11-08 15:45:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import os
-from flask import Flask
 from dotenv import load_dotenv
-from app.routes import main  # Import your blueprint
+from app import create_app
 
 # Load environment variables from config.env
 load_dotenv('config.env')
 
-# Initialize the Flask app
-app = Flask(__name__)
-
-# Register the blueprint
-app.register_blueprint(main)
+# Initialize the Flask app using the create_app function
+app = create_app()
 
 # Get BASE_URL and PORT from config.env
 BASE_URL = os.getenv('BASE_URL')
@@ -20,4 +16,3 @@ if __name__ == '__main__':
     # Use the port specified in config.env or default to 5000
     port = int(os.environ.get("PORT", PORT))
     app.run(debug=True, host='0.0.0.0', port=port)
-    
