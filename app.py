15574a9c0ebb16e1cdc857087b59466b3a0590c3
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-11-08 15:12:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ from flask import Flask
 from dotenv import load_dotenv
 from app.routes import main  # Import your blueprint
 
+# Load environment variables from config.env
 load_dotenv('config.env')
 
+# Initialize the Flask app
 app = Flask(__name__)
 
 # Register the blueprint
 app.register_blueprint(main)
 
-# Get the BASE_URL and PORT from config.env
+# Get BASE_URL and PORT from config.env
 BASE_URL = os.getenv('BASE_URL')
 PORT = os.getenv('PORT', 5000)  # Default to 5000 if PORT is not set
 
 if __name__ == '__main__':
-    # Use the port specified in config.env or default to the Heroku PORT
+    # Use the port specified in config.env or default to 5000
     port = int(os.environ.get("PORT", PORT))
     app.run(debug=True, host='0.0.0.0', port=port)
+    
